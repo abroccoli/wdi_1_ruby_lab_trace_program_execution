7c58c37473b6f74901c25d5297e44255dc2a79a9
Anthony Broccoli <acbrocco@syr.edu>
2015-01-12 14:50:11 -0500

    updated comments in triangles.rb and valid_triangle.rb

--- lib/triangle/valid_triangle.rb
+++ lib/triangle/valid_triangle.rb
@@ -1,4 +1,4 @@
-# Returns true/false if a triangle is valid
+# Returns true if a triangle is valid and false if triangle invalid
 # as per the Triangle Inequality Theorem
 def valid_triangle?(side_a, side_b, side_c)
   (side_a + side_b) > side_c &&
--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,1 +1,3 @@
+# Tell Ruby to load the code that is in the file ./triangle/valid_triangle.rb
+
 require_relative 'triangle/valid_triangle'
